5eac158415da9e0fe54412a398147f8e0d6612b1
junin275 <junin275@users.noreply.github.com>
2026-09-08 03:21:27 -0300

    Fix: Kotlin 2.0, remove Compose Compiler plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,8 +3,6 @@ pluginManagement {
         gradlePluginPortal()
         google()
         mavenCentral()
-        maven { url = uri("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/kotlin-eap") }
-        maven { url = uri("https://maven.pkg.jetbrains.space/public/p/compose/dev") }
     }
 }
 dependencyResolutionManagement {
