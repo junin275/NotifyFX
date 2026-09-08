9fc72936f21f31b371fb7c43acade64899d2926c
junin275 <junin275@users.noreply.github.com>
2026-09-08 02:41:42 -0300

    Fix: AGP 8.4.0, Kotlin 1.9.20, Compose Compiler 1.5.13

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.5.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.25" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "1.5.10" apply false
+    id("com.android.application") version "8.4.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "1.5.13" apply false
     id("com.google.dagger.hilt.android") version "2.51" apply false
 }
 
