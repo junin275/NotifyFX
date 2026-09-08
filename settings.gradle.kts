640f9ead3ab2604731ff43c541b8087d41a122b8
junin275 <junin275@users.noreply.github.com>
2026-09-08 01:52:10 -0300

    Fix: Add JetBrains plugin repository for Compose Compiler

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,7 @@ pluginManagement {
         gradlePluginPortal()
         google()
         mavenCentral()
+        maven { url = uri("https://maven.pkg.jetbrains.space/public/p/compose/dev") }
     }
 }
 dependencyResolutionManagement {
