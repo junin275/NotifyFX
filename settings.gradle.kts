d203443eeb7704ecd198df70077d8019c0159696
junin275 <junin275@users.noreply.github.com>
2026-09-08 02:52:21 -0300

    Fix: Add Kotlin EAP repository for Compose Compiler

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,7 @@ pluginManagement {
         gradlePluginPortal()
         google()
         mavenCentral()
+        maven { url = uri("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/kotlin-eap") }
         maven { url = uri("https://maven.pkg.jetbrains.space/public/p/compose/dev") }
     }
 }
